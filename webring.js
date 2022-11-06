7a4c17e39e73635870974c2c21ddeb52e88a6089
Paul Stefko <ham2anv@gmail.com>
2022-11-06 11:10:08 -0500

    Calculate text and links from JSON.

--- webring.js
+++ webring.js
@@ -1,47 +1,61 @@
-const source = "source.json"
-
 class Webring extends HTMLElement {
     constructor() {
         super();
-
-        // fetch(source)
-        //     .then(response => response.json())
-        //     .then(value => {this.ringData=value;this.make()});
     }
 
     connectedCallback() {
-        fetch(this.getAttribute("source"))
-        .then(response => response.json())
-        .then(value => {this.ringData=value;this.make()});
+        if (this.hasAttribute("source")) {
+            fetch(this.getAttribute("source"))
+            .then(response => response.json())
+            .then(value => {
+                this.ringData=value;
+                this.make()
+            });
+        }
     }
 
     make() {
-        console.log(this.ringData);
+        // const currentIndex = this.ringData.sites.indexOf(this.ringData.sites.find(({url}) => url == window.location.url))
+        const currentIndex = this.ringData.sites.indexOf(this.ringData.sites.find(({url}) => url == "https://www.nothingventuredgames.com/coding"))
+        let previousURL,nextUrl;
+        if (currentIndex == 0) {
+            previousURL = this.ringData.sites[this.ringData.sites.length - 1].url;
+            nextUrl = this.ringData.sites[currentIndex + 1].url;
+        } else if (currentIndex == this.ringData.sites.length - 1) {
+            previousURL = this.ringData.sites[currentIndex - 1].url;
+            nextUrl = this.ringData.sites[0].url;
+        } else {
+            previousURL = this.ringData.sites[currentIndex - 1].url;
+            nextUrl = this.ringData.sites[currentIndex + 1].url;
+        }
+
         
         const shadow = this.attachShadow({mode: "open"});
 
         const ring = createElement('div','wr-ring');
 
         const title = createElement('div','wr-title');
-        title.innerText = "[Webring Title]";
+        title.innerText = this.ringData.title;
 
         const current = createElement('div','wr-current');
-        current.innerText = "You are on: [current page]";
+        current.innerText = `You are on: ${document.title}`;
 
         const transfer = createElement('nav','wr-transfer');
         
+        
         const previous = createElement('div','wr-previous');
-        const prevLink = createElement('a','wr-link',{href: '#'})
+        const prevLink = createElement('a','wr-link',{href: previousURL})
         prevLink.innerText = "Previous";
         previous.append(prevLink);
 
         const next = createElement('div','wr-next');
-        const nextLink = createElement('a','wr-link',{href: '#'})
+
+        const nextLink = createElement('a','wr-link',{href: nextUrl})
         nextLink.innerText = "Next";
         next.append(nextLink);
 
         const list = createElement('div','wr-list');
-        const listLink = createElement('a','wr-link',{href: '#'})
+        const listLink = createElement('a','wr-link',{href: this.ringData.list})
         listLink.innerText = "List";
         list.append(listLink);
 
